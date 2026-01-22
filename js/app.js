ee2601bc8a4723dd9b09ca6a49959aea244f1bfc
Heba Sajid <hsajid35467@gmail.com>
2026-01-22 11:23:29 -0700

    form submission requirement added on js and storing image in memory.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,49 @@ function initApp() {
 }
 
 window.onload = initApp;
+
+const form = document.getElementById("landmarkForm");
+const landmarkList = document.getElementById("landmarkList");
+const useLocationBtn = document.getElementById("useLocation");
+
+//form submission
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value.trim();
+  const description = document.getElementById("description").value.trim();
+  const photoFile = document.getElementById("photo").files[0];
+
+  let lat = document.getElementById("lat").value;
+  let lng = document.getElementById("lng").value;
+
+  // Determine location source
+  let location = selectedLocation;
+
+  if (!location && lat && lng) {
+    location = {
+      lat: parseFloat(lat),
+      lng: parseFloat(lng)
+    };
+  }
+
+  if (!location) {
+    alert("Please provide a location");
+    return;
+  }
+
+  //storing image in memory:
+    const imageURL = URL.createObjectURL(photoFile);
+    const landmark = {
+    id: Date.now(),
+    title,
+    description,
+    imageURL,
+    location
+  };
+   landmarks.push(landmark);
+  renderLandmarks();
+
+  form.reset();
+  selectedLocation = null;
+});
